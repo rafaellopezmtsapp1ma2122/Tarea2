f39d24c3d431794150075ca1810cc1148edc10c1
Rafa <rafael_lopez_m-tsapp1ma2122@cev.com>
2023-01-19 11:00:18 +0100

    Last update

--- Tarea2/objeto.swift
+++ Tarea2/objeto.swift
@@ -15,16 +15,43 @@ class Eventos: Codable {
         //Parseamos los datos
         fechasinformato = json["date"] as? Double ?? 0
         
+       
         let dateFromater = DateFormatter()
-        let fechaparaformater = Date(timeIntervalSince1970: fechasinformato/1000)
+        if fechasinformato > 1000000000000 && fechasinformato < 1000000000000000{
+            
+             let fechaparaformater = Date(timeIntervalSince1970: fechasinformato/1000)
+            
+            dateFromater.timeZone = TimeZone(abbreviation: "OMT")
+            
+            dateFromater.locale = NSLocale.current
+            
+            dateFromater.dateFormat = "DD-MM-YYYY HH:mm"
+            
+           expE = dateFromater.string(from: fechaparaformater)
+        }
+        else if fechasinformato > 1000000000000000{
+             let fechaparaformater = Date(timeIntervalSince1970: fechasinformato/1000000)
+            dateFromater.timeZone = TimeZone(abbreviation: "OMT")
+            
+            dateFromater.locale = NSLocale.current
+            
+            dateFromater.dateFormat = "DD-MM-YYYY HH:mm"
+            
+           expE = dateFromater.string(from: fechaparaformater)
+        }
+        else {
+          
+            let fechaparaformater = Date(timeIntervalSince1970: fechasinformato)
+            dateFromater.timeZone = TimeZone(abbreviation: "OMT")
+            
+            dateFromater.locale = NSLocale.current
+            
+            dateFromater.dateFormat = "DD-MM-YYYY HH:mm"
+            
+           expE = dateFromater.string(from: fechaparaformater)
+        }
         
-        dateFromater.timeZone = TimeZone(abbreviation: "OMT")
-        
-        dateFromater.locale = NSLocale.current
-        
-        dateFromater.dateFormat = "DD-MM-YYYY HH:mm"
-        
-       expE = dateFromater.string(from: fechaparaformater)
+       
         
         
     }
